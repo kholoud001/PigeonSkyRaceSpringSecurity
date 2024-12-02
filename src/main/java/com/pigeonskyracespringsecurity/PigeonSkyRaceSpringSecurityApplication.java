a06edca8d9af1e560e53f6b50f3f5f198e2a86e2
kholoud001 <kholoud.sanak@gmail.com>
2024-12-02 10:03:19 +0100

    init

---
+++ src/main/java/com/pigeonskyracespringsecurity/PigeonSkyRaceSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pigeonskyracespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonSkyRaceSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigeonSkyRaceSpringSecurityApplication.class, args);
+    }
+
+}
