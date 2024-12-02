fa7d705b838407a868b4c46511ff6eec7dda00ea
kholoud001 <kholoud.sanak@gmail.com>
2024-12-02 11:40:59 +0100

    Implémentation d'un CustomAuthenticationProvider

---
+++ src/main/java/com/pigeonskyracespringsecurity/service/CustomUserDetailService.java
@@ -0,0 +1,34 @@
+package com.pigeonskyracespringsecurity.service;
+
+import com.pigeonskyracespringsecurity.model.User;
+import com.pigeonskyracespringsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user=  userRepository.findByUsername(username);
+
+        if(user.isPresent()){
+            var userObj = user.get();
+
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .build();
+        }else{
+            throw new UsernameNotFoundException(username);
+        }
+    }
+}
